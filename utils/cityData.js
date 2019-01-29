7fbb723762720b5e06d7f269b911a1ebdf4af132
Kylie Stanley <knstanley11@gmail.com>
2019-01-28 18:47:54 -0700

    Add data for cities

---
+++ utils/cityData.js
@@ -0,0 +1,190 @@
+const cityData = [
+  {
+    name: 'New York',
+    state: 'NY',
+    population: 8622698,
+    capital: false 
+  },
+  {
+    name: 'Los Angeles',
+    state: 'CA',
+    population: 3999759,
+    capital: false 
+  },
+  {
+    name: 'Chicago',
+    state: 'IL',
+    population: 2716450,
+    capital: false 
+  },
+  {
+    name: 'Houston',
+    state: 'TX',
+    population: 2,312,717,
+    capital: false 
+  },
+  {
+    name: 'Phoenix',
+    state: 'AZ',
+    population: 1,626,078,
+    capital: true 
+  },
+  {
+    name: 'Philadelphia',
+    state: 'PA',
+    population: 1,580,863,
+    capital: false 
+  },
+  {
+    name: 'San Antonio',
+    state: 'TX',
+    population: 1,511,946,
+    capital: false 
+  },
+  {
+    name: 'San Diego',
+    state: 'CA',
+    population: 1,419,516,
+    capital: false 
+  },
+  {
+    name: 'Dallas',
+    state: 'TX',
+    population: 1,341,075,
+    capital: false 
+  },
+  {
+    name: 'San Jose',
+    state: 'CA',
+    population: 1,035,317,
+    capital: false 
+  },
+  {
+    name: 'Austin',
+    state: 'TX',
+    population: 950,715,
+    capital: true 
+  },
+  {
+    name: 'Jacksonville',
+    state: 'FL',
+    population: 892,062,
+    capital: false 
+  },
+  {
+    name: 'San Francisco',
+    state: 'CA',
+    population: 884,363,
+    capital: false 
+  },
+  {
+    name: 'Columbus',
+    state: 'OH',
+    population: 879,170,
+    capital: true 
+  },
+  {
+    name: 'Fort Worth',
+    state: 'TX',
+    population: 874,168,
+    capital: false 
+  },
+  {
+    name: 'Indianapolis',
+    state: 'IN',
+    population: 863,002,
+    capital: true 
+  },
+  {
+    name: 'Charlotte',
+    state: 'NC',
+    population: 859,035,
+    capital: false 
+  },
+  {
+    name: 'Seattle',
+    state: 'WA',
+    population: 724,745,
+    capital: false 
+  },
+  {
+    name: 'Denver',
+    state: 'CO',
+    population: 704,621,
+    capital: true 
+  },
+  {
+    name: 'Boston',
+    state: 'MA',
+    population: 685,094,
+    capital: true 
+  },
+  {
+    name: 'El Paso',
+    state: 'TX',
+    population: 683,577,
+    capital: false 
+  },
+  {
+    name: 'Detroit',
+    state: 'MI',
+    population: 673,104,
+    capital: false 
+  },
+  {
+    name: 'Nashville',
+    state: 'TN',
+    population: 667,560,
+    capital: true 
+  },
+  {
+    name: 'Memphis',
+    state: 'TN',
+    population: 652,236,
+    capital: false 
+  },
+  {
+    name: 'Portland',
+    state: 'OR',
+    population: 647,805,
+    capital: false 
+  },
+  {
+    name: 'Oklahoma City',
+    state: 'OK',
+    population: 643,648,
+    capital: true 
+  },
+  {
+    name: 'Las Vegas',
+    state: 'NV',
+    population: 641,676,
+    capital: false 
+  },
+  {
+    name: 'Louisville',
+    state: 'KY',
+    population: 621,349,
+    capital: false 
+  },
+  {
+    name: 'Baltimore',
+    state: 'MD',
+    population: 611,648,
+    capital: false 
+  },
+  {
+    name: 'Milwaukee',
+    state: 'WI',
+    population: 595,351,
+    capital: false 
+  },
+  {
+    name: 'Kansas City',
+    state: 'MO',
+    population: 488,943,
+    capital: false 
+  },
+]
+
+module.exports = cityData
