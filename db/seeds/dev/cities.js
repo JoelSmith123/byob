6d6d2ffe430f7c76a3c4d033ef264fa32109895d
Kylie Stanley <knstanley11@gmail.com>
2019-01-29 13:32:44 -0700

    Add data to seed file

--- db/seeds/dev/cities.js
+++ db/seeds/dev/cities.js
@@ -1,13 +1,47 @@
+const cityData = require('../../cityData')
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+
+const createCity = (knex, city) => {
+  return knex('cities').insert({
+    name: city.name,
+    state: city.state,
+    capital: city.capital,
+  }, 'id')
+  .then(cityId => {
+    let restaurantPromises = [];
+
+    city.restaurants.forEach(restaurant => {
+      restaurantPromises.push(
+        createRestaurant(knex, {
+          name: restaurant.name,
+          address: restaurant.address,
+          city: restaurant.city,
+          rating: restaurant.rating,
+          avg_cost: restaurant.avg_cost,
+          city_id: cityId[0]
+        })
+      )
     });
+
+    return Promise.all(restaurantPromises);
+  })
+};
+
+const createRestaurant = (knex, restaurant) => {
+  return knex('taco_restaurants').insert(restaurant);
+};
+
+exports.seed = (knex, Promise) => {
+  return knex('taco_restaurants').del() // delete footnotes first
+    .then(() => knex('cities').del()) // delete all papers
+    .then(() => {
+      let cityPromises = [];
+
+      cityData.forEach(city => {
+        cityPromises.push(createCity(knex, city));
+      });
+
+      return Promise.all(cityPromises);
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
 };
