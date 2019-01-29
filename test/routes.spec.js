4fe1c50a06d693e181e018913b663dc0be7806fb
Joel Smith <joelwsmith123@gmail.com>
2019-01-29 14:31:38 -0700

    Add POST test for /api/v1/cities

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -36,5 +36,30 @@ describe('API Routes', () => {
         done()
       })
     })
+
+    it('POST a city to the cities array', (done) => {
+      chai.request(server)
+      .post('/api/v1/cities')
+      .send({
+        name: 'Hogsmeade',
+        state: 'n/a',
+        population: 2000,
+        capital: false
+      })
+      .end((error, response) => {
+        response.should.have.status(201)
+        response.should.be.json
+        response.body.should.be.a('object')
+        response.body.should.have.property('name')
+        response.body.name.should.equal('Hogsmeade')
+        response.body.should.have.property('state')
+        response.body.state.should.equal('n/a')
+        response.body.should.have.property('population')
+        response.body.population.should.equal(2000)
+        response.body.should.have.property('capital')
+        response.body.capital.should.equal(false)
+        done()
+      })
+    })
   })
 })
