4a3cb9ca07bf19b272b2dc0a624910a9672f078c
Joel Smith <joelwsmith123@gmail.com>
2019-01-29 14:24:50 -0700

    Add GET test for /api/v1/cities

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -17,5 +17,24 @@ describe('Client Routes', () => {
 })
 
 describe('API Routes', () => {
-  
+  describe('/api/v1/cities', () => {
+    it('GET array all cities', (done) => {
+      chai.request(server)
+      .get('/api/v1.cities')
+      .end((error, response) => {
+        response.should.have.status(200)
+        response.should.be.json
+        response.body.should.be.a('array')
+        response.body[0].should.have.property('name')
+        response.body[0].name.should.equal('New York City')
+        response.body[0].should.have.property('state')
+        response.body[0].state.should.equal('NY')
+        response.body[0].should.have.property('population')
+        response.body[0].population.should.equal(8622698)
+        response.body[0].should.have.property('capital')
+        response.body[0].capital.should.equal(false)
+        done()
+      })
+    })
+  })
 })
