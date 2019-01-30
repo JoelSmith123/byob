5b4045c5105edef600f932e026beb7f292c6e579
Kylie Stanley <knstanley11@gmail.com>
2019-01-29 17:21:08 -0700

    Add endpoints to server.js

--- server.js
+++ server.js
@@ -8,4 +8,123 @@ const database = require('knex')(configuration);
 app.use( bodyParser.json() )
 
 app.set('port', process.env.PORT || 3000)
-app.locals.title = 'Pet Box'
+app.locals.title = 'Taco Restaurants'
+
+
+
+app.get('/api/v1/cities', (request, response) => {
+  database('cities').select()
+  .then((cities) => {
+    response.status(200).json(cities);
+  })
+  .catch((error) => {
+    response.status(500).json({ error });
+  });
+})
+
+app.post('/api/v1/cities', (request, response) => {
+  const project = request.body
+
+  for (let requiredParameter of ['name, state, population, restaurants'] && typeof request.body.capital !== 'undefined') {
+    if (!project[requiredParameter]) {
+      return response.status(422)
+        .send({ error: `Expected format: { name: <String>, state: <String>, population: <String>, capital: <Boolean>, 
+                restaurants: <Array> }. You're missing a "${requiredParameter}" property.` });
+    }
+
+  database('cities').insert(city, 'id')
+  .then(city => {
+    response.status(201).json({ id: city[0] })
+  })
+  .catch(error => {
+    response.status(500).json({ error })
+  })
+})
+
+
+
+app.get('/api/v1/restaurants', (request, response) => {
+  database('restaurants').select()
+  .then((restaurants) => {
+    response.status(200).json(restaurants);
+  })
+  .catch((error) => {
+    response.status(500).json({ error });
+  });
+})
+
+
+
+app.get('/api/v1/cities/:city_id', (request, response) => {
+  const { city_id } = request.params
+
+  database('cities').where('id', city_id).select()
+  .then(city => response.status(200).json(city))
+})
+
+
+app.put('/api/v1/cities/:city_id', (request, response) => {
+
+})
+
+
+app.delete('/api/v1/cities/:city_id', (request, response) => {
+ 
+})
+
+
+
+app.get('/api/v1/restaurants/:id', (request, response) => {
+  const { id } = request.params
+
+  database('restaurants').where('id', id).select()
+  .then(restaurant => response.status(200).json(restaurant))
+})
+
+
+
+app.put('/api/v1/restaurants/:id', (request, response) => {
+
+})
+
+
+
+app.delete('/api/v1/restaurants/:id', (request, response) => {
+  database('restaurants').where('id', request.params.id).delete()
+  .then(restaurant => {
+     response.status(201).json({ id: restaurant[0] })
+  })
+  .catch(error => {
+    response.status(500).json({ error })
+  })
+})
+
+
+
+
+app.get('/api/v1/cities/:city_id/restaurants', (request, response) => {
+  database('restaurants').where('city_id', request.params.city_id).select()
+  .then((restaurants) => {
+    response.status(200).json(restaurants);
+  })
+  .catch((error) => {
+    response.status(500).json({ error });
+  });
+})
+
+
+app.post('/api/v1/cities/:city_id/restaurants', (request, response) => {
+  const restaurant = request.body
+  const { city_id } = request.params
+
+  database('restaurants').insert(restaurant, 'id')
+  .then(restaurant => {
+    response.status(201).json({ ...request.body, id: restaurant[0], city_id })
+  })
+  .catch(error => {
+    response.status(500).json({ error })
+  })
+})
+
+
+
